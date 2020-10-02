0c0055b1eef5f0567e7dc08be832146b9bebed6f
Ria Pande <rpande@stu.naperville203.org>
2020-10-02 14:15:41 -0500

    turtle lab 1st ring

--- TurtleLab.java
+++ TurtleLab.java
@@ -4,6 +4,16 @@ public class TurtleLab
 {
     public static void main(String[] args)
     {
-        
+        World myWorld = new World(); 
+        Turtle blue = new Turtle(myWorld); 
+        Turtle dicaprio = new Turtle(myWorld);
+        blue.setPenColor(Color.blue);
+        blue.penUp();
+        blue.moveTo(100,150);
+        blue.penDown();
+        for (int i = 0; i < 50; i++){
+            blue.forward(100);
+            blue.turn(110);
+        }
     }
 }
